378836aa583fb72065d34752d6f82836821b9e74
Jorge Santos <jorge.santos@autocargo.com.br>
2015-01-23 15:42:57 -0200

    First commit

---
+++ app/models/qualificacao.rb
@@ -0,0 +1,10 @@
+class Qualificacao < ActiveRecord::Base
+  validates_presence_of :nota, :message => ' - deve ser preenchido'
+  validates_presence_of :valor_gasto, :message => ' - deve ser preenchido'
+  validates_numericality_of :nota,  :greater_than_or_equal_to => 0, :less_than_or_equal_to => 10, :message => ' - deve ser um número entre 0 e 10'
+  validates_numericality_of :valor_gasto,  :greater_than_or_equal_to => 0, :message => ' - deve ser um número número maior que 0'
+
+  belongs_to :cliente
+  belongs_to :restaurante
+
+end
